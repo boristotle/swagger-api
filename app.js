059c59fb2ba391fc4503f6c1d5dd9b789edff2dd
Darrin Bennett <boristotle@hotmail.com>
2015-11-13 11:41:56 -0700

    finished?

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
   require('dotenv').load();
+  var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -22,9 +23,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', routes);
 app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
-var conString = "postgres://@localhost/memories";
+var dotenv = require('dotenv')
+var cors = require('cors');
+var conString = process.env.DATABASE_URL || "postgres://@localhost/memoriesapp";
 
 // GET ALL memories
 router.get('/api/v1/memories', function(req, res, next){
